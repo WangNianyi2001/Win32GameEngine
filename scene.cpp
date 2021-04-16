4d1c56b43af6bacf651503996f802e412f87c662
Nianyi Wang <WangNianyi2001@outlook.com>
2021-04-17 01:13:08 +0800

    Fix map initialization in scene.cpp

--- scene.cpp
+++ scene.cpp
@@ -2,17 +2,30 @@
 
 using namespace Win32GameEngine;
 
-Scene::Scene(char const *name) : event_handler(EventHandler()) {
-	scenes.insert(pair(name, this));
+map<string, Scene *> *Scene::all = nullptr;
+Scene *Scene::active = nullptr;
+
+Scene::Scene(string name, map<UINT, set<EventHandler::Handler>> handlers) :
+	event_handler(EventHandler(handlers))
+{
+	if(!all)
+		all = new map<string, Scene *>{};
+	(*all)[name] = this;
 }
 
-bool switchScene(char const *name, HWND hWnd) {
-	if(!scenes.count(name))
+bool Scene::switchTo(char const *name, HWND hWnd) {
+	if(!Scene::all->count(name))
 		return false;
-	if(active_scene)
-		active_scene->event_handler(hWnd, WM_DESTROY, NULL, NULL);
-	Scene *scene = scenes[name];
-	active_scene = scene;
+	if(Scene::active)
+		Scene::active->event_handler(hWnd, WM_DESTROY, NULL, NULL);
+	Scene *scene = (*Scene::all)[name];
+	Scene::active = scene;
 	scene->event_handler(hWnd, WM_CREATE, NULL, NULL);
 	return true;
+}
+
+LRESULT Scene::passEvent(HWND hWnd, UINT type, WPARAM wParam, LPARAM lParam) {
+	if(Scene::active)
+		return Scene::active->event_handler(hWnd, type, wParam, lParam);
+	return DefWindowProc(hWnd, type, wParam, lParam);
 }
--- scene.h
+++ scene.h
@@ -2,17 +2,20 @@
 
 #include "paintable.h"
 #include "window.h"
+#include <string>
 #include <map>
+#include <set>
 
 namespace Win32GameEngine {
 	using namespace std;
 
 	class Scene {
 	public:
+		static map<string, Scene *> *all;
+		static Scene *active;
 		EventHandler event_handler;
-		Scene(char const *name);
+		Scene(string name, map<UINT, set<EventHandler::Handler>> handlers = {});
+		static bool switchTo(char const *name, HWND hWnd = NULL);
+		static LRESULT passEvent(HWND hWnd, UINT type, WPARAM wParam, LPARAM lParam);
 	};
-	static map<char const *, Scene *> scenes;
-	static Scene *active_scene = nullptr;
-	bool switchScene(char const *name, HWND hWnd = NULL);
 }
