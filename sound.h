1c002af13b64f1b0331278a7a1a1facdc1b0577b
Nianyi Wang <WangNianyi2001@outlook.com>
2021-04-18 18:57:05 +0800

    Sound options

--- sound.h
+++ sound.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <windows.h>
+#include <mmsystem.h>
+
 namespace Win32GameEngine {
-	void playSound(wchar_t const *url);
+	void playSound(wchar_t const *url, int option = SND_ASYNC);
+	void playSoundEffect(wchar_t const *url);
 }
