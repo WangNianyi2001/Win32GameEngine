8ef16b82c8b79400f72e7c747ba9cb8b46d2ee18
Nianyi Wang <Wangnianyi2001@outlook.com>
2021-12-16 18:45:54 +0800

    Remaster creation of game objects

--- game.hpp
+++ game.hpp
@@ -14,6 +14,65 @@ namespace Win32GameEngine {
 	class Entity;
 	class Component;
 
+	// Components are the abilities an entity have.
+	class Component : public GameObject {
+	public:
+		Entity *entity;
+		Component(Entity *entity) : entity(entity) {}
+	};
+
+	// Game objects that can exist in a scene.
+	class Entity : public GameObject {
+	public:
+		Scene *scene;
+		set<Component *> components;
+		Entity *parent;
+		Transform transform;
+		Entity(Scene *scene) : scene(scene) {}
+		virtual ~Entity() {
+			for(Component *component : components)
+				delete component;
+		}
+		void addcomponent(Component *component) { components.insert(component); }
+		template<derived_from<Component> Component, typename ...Args>
+		inline Component *makecomponent(Args ...args) {
+			Component *component = new Component(this, args...);
+			addcomponent(component);
+			return component;
+		}
+	};
+
+	// Physical separation of entities.
+	class Scene : public GameObject {
+	public:
+		Game *game;
+		vector<Entity *> entities;
+		Scene(Game *game) : game(game) {
+			// Sort by Z coordinate when update
+			add(GameEventType::UPDATE, [&](GameEvent) {
+				sort(entities.begin(), entities.end());
+			});
+		}
+		virtual ~Scene() {
+			for(Entity *entity : entities)
+				delete entity;
+		}
+		void addentity(Entity *entity) {
+			// Insert the new entity to the correct Z-ordered place.
+			auto it = entities.begin();
+			for(auto end = entities.end(); it != end; ++it) {
+				if(entity < *it)
+					break;
+			}
+			entities.insert(it, entity);
+		}
+		inline Entity *makeentity() {
+			Entity *entity = new Entity(this);
+			addentity(entity);
+			return entity;
+		}
+	};
+
 	// The instance of the entire game, communicates with the Win32 API,
 	// handles scene management, provides a runtime-long environment for
 	// game objects to interact internally with.
@@ -45,78 +104,11 @@ namespace Win32GameEngine {
 			window->events.add(WM_DESTROY, [&](SystemEvent) { inactivate(); });
 			window->events.add(WM_QUIT, [&](SystemEvent) { PostQuitMessage(0); });
 		}
-		void addscene(Scene *scene) {
-			scenes.insert(scene);
-		}
-	};
-
-	// Physical separation of entities.
-	class Scene : public GameObject {
-	public:
-		Game *game;
-		vector<Entity *> entities;
-		Scene(Game *game) : game(game) {
-			game->addscene(this);
-			// Sort by Z coordinate when update
-			add(GameEventType::UPDATE, [&](GameEvent) {
-				sort(entities.begin(), entities.end());
-			});
-		}
-		virtual ~Scene() {
-			for(Entity *entity : entities)
-				delete (Receiver<GameEvent> *)entity;
-		}
-		void addentity(Entity *entity) {
-			// Insert the new entity to the correct Z-ordered place.
-			auto it = entities.begin();
-			for(auto end = entities.end(); it != end; ++it) {
-				if(entity < *it)
-					break;
-			}
-			entities.insert(it, entity);
-		}
-		template<derived_from<Component> Component>
-		inline Entity *makesingular() {
-			return (new Component(new Entity(this)))->entity;
-		}
-	};
-
-	// Game objects that can exist in a scene.
-	class Entity : public GameObject {
-	public:
-		Scene *scene;
-		set<Component *> components;
-		Entity *parent;
-		set<Entity *>children;
-		Transform transform;
-		Entity(Scene *scene) : scene(scene) {
-			scene->addentity(this);
-		}
-		virtual ~Entity() {
-			for(Component *component : components)
-				delete (Receiver<GameEvent> *)component;
-		}
-		// Less in Z coordinate
-		bool operator<(Entity const &entity) const {
-			return transform.position.at(2) < entity.transform.position.at(2);
-		}
-		void setparent(Entity *entity) {
-			if(parent)
-				parent->children.erase(this);
-			if(parent = entity)
-				parent->children.insert(this);
-		}
-		void addcomponent(Component *component) {
-			components.insert(component);
-		}
-	};
-
-	// Components are the abilities an entity have.
-	class Component : public GameObject {
-	public:
-		Entity *entity;
-		Component(Entity *entity) : entity(entity) {
-			entity->addcomponent(this);
+		void addscene(Scene *scene) { scenes.insert(scene); }
+		inline Scene *makescene() {
+			Scene *scene = new Scene(this);
+			addscene(scene);
+			return scene;
 		}
 	};
 }
